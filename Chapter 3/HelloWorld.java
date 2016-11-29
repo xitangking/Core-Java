2eff54a81c640bc89cdcddd79dac7fc4fc3dad38
xtw2016 <15220815316@163.com>
2016-11-29 15:23:23 +0800

    开始Java之旅
    HelloWorld

---
+++ Chapter 3/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+  This is the frist sample program in Core Java Chapter 3
+  @version 0.0.1 2016-11-29
+  @author 西唐王
+*/
+
+  public class HelloWorld
+  {
+  	public static void main(String[] args) 
+  	{
+  	  	System.out.println("We will not use 'Hello,World!'");
+  	}  
+  }
+
+
+
